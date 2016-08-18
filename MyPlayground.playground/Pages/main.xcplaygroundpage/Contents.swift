39ed928c3a9ed9edf5358cf07109b761538bd272
jicaey <young.ma@me.com>
2016-08-18 12:22:03 -0400

    Lab Complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,10 +13,11 @@
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func numberOfCountries(continent: String, number: Int){
+    print("\(continent) is a continent which contains \(number) countries.")
+}
 
-
-
+numberOfCountries("Africa", number: 54)
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,15 +26,12 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
-
-
-
+greeting("Danny", greeting: "Hello")
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,10 +43,11 @@ daysInMonth("November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottlesOnWall(number: Int, bottles: String, wall: String){
+    print("\(number) bottles of \(bottles) on the \(wall) wall")
+}
 
-
-
+bottlesOnWall(99, bottles: "beer", wall: "brick")
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
